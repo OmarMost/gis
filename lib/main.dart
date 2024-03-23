fdc695545d02290f7ad22a7c11bd288dfe63e3c4
Leader <abdo.gammal1233@gmail.com>
2024-03-23 10:06:10 +0200

    Hi, Image Profile

--- lib/main.dart
+++ lib/main.dart
@@ -46,12 +46,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       home: Onboarding(), //Put Onboarding to normal Start.
-
-    
       theme: ThemeData(fontFamily: 'DidactGothic-Regular'),
-    
     );
   }
 }
