350f5038df3d3339c8ccac18f7f289f0d3e9f652
Leader <abdo.gammal1233@gmail.com>
2024-05-10 22:00:54 +0300

    Date And Ti

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'dart:collection';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:gis/firebase_options.dart';
+import 'package:gis/Screens/onboarding_Screens/on_boarding.dart';
+
 import 'package:gis/Screens/LoginScreens/auth_page.dart';
 import 'package:gis/Screens/LoginScreens/privacy.dart';
 import 'package:gis/Screens/LoginScreens/privacy.dart';
@@ -10,13 +13,11 @@ import 'package:gis/Screens/SecurtyScreens/reportSOS.dart';
 import 'package:gis/Screens/StudentScreens/choose_bulding.dart';
 import 'package:gis/Screens/LoginScreens/signup.dart';
 import 'package:gis/Screens/StudentScreens/old_reports.dart';
-
 import 'package:gis/Screens/StudentScreens/student_home.dart';
 import 'package:gis/Screens/StudentScreens/location_details.dart';
 import 'package:gis/Screens/StudentScreens/sos_page.dart';
 import 'package:gis/Screens/StudentScreens/report_problem_page.dart';
 import 'package:gis/Screens/StudentScreens/first_aid_page.dart';
-
 import 'package:gis/Screens/StudentScreens/student_home.dart'; //My Page [StudentHome]..Elgammal
 import 'package:gis/Screens/StudentScreens/location_details.dart';
 import 'package:gis/Screens/LoginScreens/sec_login.dart'; // ..Elgammal
@@ -24,11 +25,7 @@ import 'package:gis/Screens/StudentScreens/sos_page.dart'; // ..Elgammal
 import 'package:gis/Screens/StudentScreens/report_problem_page.dart'; // ..Elgammal
 import 'package:gis/Screens/StudentScreens/first_aid_page.dart'; // .. Elgammal
 import 'package:gis/Screens/SecurtyScreens/map.dart'; // to test map
-import 'package:gis/Screens/onboarding_Screens/on_boarding.dart';
 import 'package:gis/Screens/onboarding_Screens/type_selection.dart';
-
-import 'package:gis/firebase_options.dart';
-
 import 'package:gis/Screens/StudentScreens/profile.dart';
 import 'package:gis/Screens/StudentScreens/profile_Help.dart';
 import 'package:gis/Screens/LoginScreens/loginScreen.dart';
@@ -42,7 +39,6 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
